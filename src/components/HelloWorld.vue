aa5d544c5f252e6a24bb1416a1a06f409c6eff6d
Maxim Dolgov <mdolgov@ozon.ru>
2018-08-08 17:01:31 +0300

    svg preview

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,112 +1,62 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a
-          href="https://vuejs.org"
-          target="_blank"
-        >
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://forum.vuejs.org"
-          target="_blank"
-        >
-          Forum
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://chat.vuejs.org"
-          target="_blank"
-        >
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://twitter.com/vuejs"
-          target="_blank"
-        >
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a
-          href="http://vuejs-templates.github.io/webpack/"
-          target="_blank"
-        >
-          Docs for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a
-          href="http://router.vuejs.org/"
-          target="_blank"
-        >
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vuex.vuejs.org/"
-          target="_blank"
-        >
-          vuex
-        </a>
-      </li>
-      <li>
-        <a
-          href="http://vue-loader.vuejs.org/"
-          target="_blank"
-        >
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-        >
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+	<div class="hello">
+		<h1>{{ msg }}</h1>
+		<input type="file" @change="readFile($event)"><br>
+		<img :src="imgData" height="200" title="Image preview..." v-if="imgData">
+	</div>
 </template>
 
 <script>
+let reader = new FileReader();
 export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'bxt store'
-    }
-  }
+	name: 'HelloWorld',
+	data () {
+		return {
+			msg: 'title',
+			imgData: ''
+		}
+	},
+	methods: {
+		readFile($event) {
+      console.log(' * $event: ', $event);
+      const fileInput = $event && $event.target;
+      console.log(' * fileInput : ', fileInput);
+      let file = fileInput && fileInput.files && fileInput.files.length && fileInput.files[0];
+      // if (!file) { return; }
+
+		  let reader = new FileReader();
+      reader.onloadend = () => {
+        console.log(' * reader.result: ', reader.result);
+        this.imgData = reader.result;
+      };
+      console.log(' * reader: ', reader);
+      console.log(' * file: ', file);
+      if (file) {
+        reader.readAsDataURL(file);
+      }
+      else {
+        this.imgData = '';
+      }
+
+
+		}
+	}
 }
 </script>
 
 <style scoped>
 h1, h2 {
-  font-weight: normal;
+	font-weight: normal;
 }
 ul {
-  list-style-type: none;
-  padding: 0;
+	list-style-type: none;
+	padding: 0;
 }
 li {
-  display: inline-block;
-  margin: 0 10px;
+	display: inline-block;
+	margin: 0 10px;
 }
 a {
-  color: #42b983;
+	color: #42b983;
 }
 </style>
