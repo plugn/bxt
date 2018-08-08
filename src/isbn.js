67c77ab5528a7a309297d9b32b4fe292925c5957
Maxim Dolgov <mdolgov@ozon.ru>
2018-08-08 21:45:06 +0300

    isbn utils start

--- src/isbn.js
+++ src/isbn.js
@@ -13,7 +13,7 @@ export const ISBN10 = {
 		let list = String(str).split(''),
 			count = list.length;
 
-		if (count !== 10) { throw new Error(' (!) ISBN10::parse() chunks count !== 10'); }
+		if (! [9, 10].includes(Number(count))) { throw new Error(' (!) ISBN10::parse() chunks count !== 10'); }
 
 
 		return list;
@@ -31,6 +31,18 @@ export const ISBN10 = {
 		if (list.length === 10) {
 			givenCheckDigit = list.pop();
 		}
+
+		if (list.length !== 9) {
+			throw new Error(' (!) calcCheckDigit() ILLEGAL list.length: ' + list.length);
+		}
+
+		const reducer = (acc, chunk, index) => {
+			acc += (10 - index) * Number(chunk);
+			return acc;
+		};
+
+		return  list.reduce(reducer, 0)
+
 
 	}
 };
