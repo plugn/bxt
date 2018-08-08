62e1d784d7fe0dd24f8c26731249860fff50b03a
Maxim Dolgov <mdolgov@ozon.ru>
2018-08-08 22:19:47 +0300

    ISBN10 validator

--- src/isbn.js
+++ src/isbn.js
@@ -10,7 +10,7 @@ export const ISBN10 = {
 	 * @returns {string[]}
 	 */
 	parse(str) {
-		let list = String(str).split(''),
+		let list = String(str).replace(/^ISBN|[\s-:]+/gi, '').split(''),
 			count = list.length;
 
 		if (! [9, 10].includes(Number(count))) { throw new Error(' (!) ISBN10::parse() chunks count !== 10'); }
@@ -19,31 +19,38 @@ export const ISBN10 = {
 		return list;
 	},
 
-	replaceX(lastItem) {
-		return lastItem.toLowerCase() === 'x' ? '10' : lastItem;
-	},
-
-	calcCheckDigit(parsedList) {
-		// work with a clone to avoid side-effects
-		let list = [].concat(parsedList),
-			givenCheckDigit = '';
-
-		if (list.length === 10) {
-			givenCheckDigit = list.pop();
-		}
-
-		if (list.length !== 9) {
-			throw new Error(' (!) calcCheckDigit() ILLEGAL list.length: ' + list.length);
-		}
-
+	calcCheckDigit(list) {
 		const reducer = (acc, chunk, index) => {
 			acc += (10 - index) * Number(chunk);
 			return acc;
 		};
+		const sum = list.reduce(reducer, 0);
+		const remain = 11 - (sum % 11);
+		
+		return remain === 10 ? 'x' : String(remain);
+	},
+	
+	validate(str) {
+		try {
+			const chunks = this.parse(str);
+			// console.log(' * validate() chunks: ', chunks);
+			if (chunks.length !== 10) {
+				return false;
+			}
 
-		return  list.reduce(reducer, 0)
+			const givenCheckDigit = chunks.pop();
+			const checkDigit = this.calcCheckDigit(chunks);
+
+			return givenCheckDigit.toLowerCase() === checkDigit;
+
+		} catch (e) {
+			console.warn(' (!) ISBN10::validate(' + str + ') failed: ' + e.message);
+
+			return false;
+		}
 
 
 	}
+	
 };
 
