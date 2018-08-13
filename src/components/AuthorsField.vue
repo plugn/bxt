b7d36f4a72a0383453ec7049e9160d88cfb77177
Maxim Dolgov <mdolgov@ozon.ru>
2018-08-13 21:59:39 +0300

    authors auto add field and buttons way

--- src/components/AuthorsField.vue
+++ src/components/AuthorsField.vue
@@ -6,12 +6,14 @@
 
 		<input type="text" placeholder="First Name" :value="author.firstName" >
 		<input type="text" placeholder="Last Name" :value="author.lastName" >
+
+		<span v-if="index===0" class="pure-form-message-inline"> <button> + </button> <button> - </button></span>
 	</div>
 	<div class="pure-control-group" v-if="isListFilled">
 		<label for="newInput">(fill to add)&nbsp;</label>
 
-		<input id="newInput" type="text" placeholder="First Name" :value="newAuthor.firstName" >
-		<input type="text" placeholder="Last Name" :value="newAuthor.lastName" >
+		<input id="newInput" type="text" placeholder="First Name" v-model="newAuthor.firstName" >
+		<input type="text" placeholder="Last Name" v-model="newAuthor.lastName" >
 	</div>
 
 </div>
@@ -46,19 +48,38 @@ export default {
 
 	computed: {
 		isListFilled() {
-			return this.isFieldValid(this.value);
+			return this.isListValid(this.value);
 		}
 	},
 	mounted() {
 		console.log(' *  AuthorsField', this.value);
+
+		this.$watch('newAuthor', author => {
+			console.log(' * newAuthor author : ', JSON.stringify(author) );
+			const filled = this.isListValid([author]);
+			console.log(' * filled: ', filled);
+			if (filled) {
+				this.value.push({...author});
+				this.newAuthor = {firstName:'', lastName: ''}
+			}
+		}, {
+			deep: true
+		});
+
+
 	},
 	methods: {
-		isFieldValid(value) {
+		isListValid(value) {
 			return value.every(({firstName, lastName}) => (String(firstName).trim() && String(lastName).trim()) )
 		}
 	},
 	watch: {
-
+		newFirstName(val) {
+			console.log(' * newFirstName ', val);
+		},
+		newLastName(val) {
+			console.log(' * newLastName ', val);
+		}
 	}
 };
 </script>
